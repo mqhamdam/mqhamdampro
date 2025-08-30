f655b5eb28899e2a2bab0c3894417e9dff260a13
Hamdam <mq.hamdam@gmail.com>
2025-08-30 13:09:52 +0900

    fix built-time error, clean up

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,11 @@ import "./index.css";
 import { RouterProvider } from "react-router";
 import { appRouter } from "./app/app.router.tsx";
 import { ConfigProvider } from "antd";
-import { HelmetProvider } from "react-helmet-async";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HelmetProvider>
-      <ConfigProvider>
-        <RouterProvider router={appRouter} />{" "}
-      </ConfigProvider>
-    </HelmetProvider>
+    <ConfigProvider>
+      <RouterProvider router={appRouter} />{" "}
+    </ConfigProvider>
   </StrictMode>
 );
